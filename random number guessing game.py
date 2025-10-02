debd6c02349f27e7232eabeee8639c734ba3e389
Your NameYahia <poloyahia2019@gmail.com>
2025-10-02 18:37:17 +0200

    Create random # guessing game

---
+++ random number guessing game.py
@@ -0,0 +1,30 @@
+import random
+
+lowest = 1
+highest = 10
+
+while True:
+    guesses = 0
+    answer = random.randint(lowest, highest)
+
+    guess = input(f"Guess a number ({lowest}-{highest})").strip()
+
+    if guess.isdigit():
+        guess = int(guess)
+        if guess == answer:
+            print(
+                f"Congrats you picked the right number with {guesses} wrong attempts")
+            restart = input("Do you want to restart (y/n): ").lower()
+            if restart != ("y"):
+                break
+            else:
+                print("Goodbye!")
+        elif guess < answer:
+            print("Your guess was lower than the number. Try again: ")
+            guesses += 1
+        elif guess > answer:
+            print("Your guess was higher than the number. Try again: ")
+            guesses += 1
+
+    else:
+        print("Input invalid, please only eneter whole positive integers")
